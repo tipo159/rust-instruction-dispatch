f882c1c52cf8112e9ffb2cfee2adb792113e71a6
tipo159 <tipo159@gmail.com>
2023-10-25 09:29:47 +0900

    Fix optimized_direct_threadig.c. Reformat direct_threading.c

--- C/direct-threading/direct_threading.c
+++ C/direct-threading/direct_threading.c
@@ -23,15 +23,15 @@ typedef unsigned int bytecode_t;
 //    A:Imm:OP =    8:20: 4
 //  A:B:Jmp:OP = 8: 8:12: 4
 
-#define MAKE_OPCODE(op) ((op)&0xf)
+#define MAKE_OPCODE(op) ((op) & 0xf)
 
-#define MAKE_OPCODE_A_B_C(op, a, b, c) (((op)&0xf) + (((a)&0xff) << 24) + (((b)&0xff) << 16) + (((c)&0xfff) << 4))
+#define MAKE_OPCODE_A_B_C(op, a, b, c) (((op) & 0xf) + (((a) & 0xff) << 24) + (((b) & 0xff) << 16) + (((c) & 0xfff) << 4))
 
-#define MAKE_OPCODE_IMM(op, a, imm) (((op)&0xf) + (((a)&0xff) << 24) + (((imm & 0xfffff) << 4)))
+#define MAKE_OPCODE_IMM(op, a, imm) (((op) & 0xf) + (((a) & 0xff) << 24) + (((imm & 0xfffff) << 4)))
 
-#define MAKE_OPCODE_A_B_JMP(op, a, b, jmp) (((op)&0xf) + (((a)&0xff) << 24) + (((b)&0xff) << 16) + (((jmp)&0xfff) << 4))
+#define MAKE_OPCODE_A_B_JMP(op, a, b, jmp) (((op) & 0xf) + (((a) & 0xff) << 24) + (((b) & 0xff) << 16) + (((jmp) & 0xfff) << 4))
 
-#define GET_OPCODE(instruction) ((instruction)&0xf)
+#define GET_OPCODE(instruction) ((instruction) & 0xf)
 
 #define GET_OPERAND_A(instruction) (((instruction) >> 24) & 0xff)
 
@@ -72,7 +72,7 @@ int main()
         }
         clock_gettime(CLOCK_MONOTONIC_RAW, &ts_start);
         pc = 0;
-	goto *dispatch_table[GET_OPCODE(program[pc])];
+        goto *dispatch_table[GET_OPCODE(program[pc])];
     do_load:
         memory[GET_OPERAND_A(program[pc])] = GET_OPERAND_IMM(program[pc]);
         DISPATCH();
